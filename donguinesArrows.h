03e8e370960252ec5f806b752f87f643cfe2a127
jncdngs <jonchesterdonguines07@gmail.com>
2025-02-03 20:13:23 +0800

    Split code into function and header files

---
+++ donguinesArrows.h
@@ -0,0 +1,8 @@
+#ifndef DONGUINES_ARROWS_H
+#define DONGUINES_ARROWS_H
+
+void cls();
+void printTitle();
+void printDivider();
+
+#endif
--- donguinesFirstInit.c
+++ donguinesFirstInit.c
@@ -14,33 +14,7 @@ students and/or persons.
 #include <stdio.h>
 #include <conio.h>  // for getch()
 #include <string.h>
-
-void cls()
-{
-    printf("\033[H\033[J");
-}
-
-void printTitle()
-{
-    printf(" ______          _                                     _____                                  __\n");
-    printf(" \\ \\ \\ \\        / \\   _ __ _ __ _____      _____      | ____|_  ___ __  _ __ ___  ___ ___     \\ \\\n");
-    printf("  \\ \\ \\ \\_____ / _ \\ | '__| '__/ _ \\ \\ /\\ / / __|_____|  _| \\ \\/ / '_ \\| '__/ _ \\/ __/ __|_____\\ \\\n");
-    printf("  / / / /_____/ ___ \\| |  | | | (_) \\ V  V /\\__ \\_____| |___ >  <| |_) | | |  __/\\__ \\__ \\_____/ /\n");
-    printf(" /_/_/_/     /_/   \\_\\_|  |_|  \\___/ \\_/\\_/ |___/     |_____/_/\\_\\ .__/|_|  \\___||___/___/    /_/\n");
-    printf("                                                                 |_|\n\n");
-}
-
-void printDivider()
-{
-    int i;
-    
-    for(i = 0; i < 100; i++)
-    {
-        printf("-");
-    }
-
-    printf("\n");
-}
+#include "donguinesArrows.h"
 
 int main()
 {
