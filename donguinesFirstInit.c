1eaa872c9008d71a1042480ec751f509a14461be
jncdngs <jonchesterdonguines07@gmail.com>
2025-02-19 18:15:07 +0800

    Fix viewPassengerCount() call parameters

--- donguinesFirstInit.c
+++ donguinesFirstInit.c
@@ -147,7 +147,7 @@ int main()
                 if (personnelMenuOption == 1)       // [1] View passenger count
                 {
                     cls();
-                    viewPassengerCount(passengers);
+                    viewPassengerCount(trip, passengers);
                 }
                 else if (personnelMenuOption == 2)  // [2] View drop-off count
                 {
